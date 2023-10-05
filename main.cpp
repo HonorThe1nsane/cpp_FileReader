0c356581a44e2368c003946074853086a3e1558b
HonorThe1nsane <troymd2009@hotmail.com>
2023-10-04 21:42:57 -0600

    completed

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main()
         cout << "--------------------------------" << endl;
         cout << "Would you like to play? (y/n): ";
         cin >> userPlay;
+        // cin.ignore();
         cout << endl;
 
         if (userPlay == 'Y' || userPlay == 'y')
@@ -26,35 +27,55 @@ int main()
             cout << "2. Camping MadLib" << endl;
             int userChoice = 0;
             cin >> userChoice;
-            cin.ignore();
-            cout << endl;
+
             if (userChoice == 1)
             {
                 cout << "Great choice. Time to go to the zoo!" << endl;
                 cout << "Loading file..." << endl;
-                {
 
-                    inputFile.open("madLibZoo.txt");
-                    outputFile.open("adjustMadLib.txt");
-                    break;
+                inputFile.open("madLibZoo.txt");
+                if (!inputFile.is_open())
+                {
+                    cerr << "Failed to open madLibZoo.txt" << endl;
+                    return 1;
                 }
+
+                outputFile.open("adjustMadLib.txt");
+                if (!outputFile.is_open())
+                {
+                    cerr << "Failed to open adjustMadLib.txt" << endl;
+                    return 1;
+                }
+
+                break;
             }
             else if (userChoice == 2)
             {
                 cout << "Great choice. Time to go camping!" << endl;
                 cout << "Loading file..." << endl;
-                {
+                inputFile.open("madLibCamping.txt");
 
-                    inputFile.open("madLibCamping.txt");
-                    outputFile.open("adjustMadLib.txt");
-                    break;
+                if (!inputFile.is_open())
+                {
+                    cerr << "Failed to open madLibCamping.txt" << endl;
+                    return 1;
                 }
+
+                outputFile.open("adjustMadLib.txt");
+                if (!outputFile.is_open())
+                {
+                    cerr << "Failed to open adjustMadLib.txt" << endl;
+                    return 1;
+                }
+
+                break;
             }
             else
             {
                 cout << "Sorry please choose 1 or 2" << endl;
             }
-        } else if (userPlay == 'N' || userPlay == 'n')
+        }
+        else if (userPlay == 'N' || userPlay == 'n')
         {
             cout << "Sorry you didn't want to play" << endl;
             return 0;
@@ -65,14 +86,7 @@ int main()
         }
     }
 
-    if (!inputFile.is_open() || !outputFile.is_open())
-    {
-        cerr << "Failed to open one or more files." << endl;
-        return 1;
-    }
-
-
-    regex pattern("\\(.*?\\)");  // Regular expression to match text inside parentheses
+    regex pattern("\\(([^)]+)\\)"); // Regular expression to match text inside parentheses
 
     string line;
     while (getline(inputFile, line))
@@ -86,37 +100,82 @@ int main()
                 string replacement;
 
                 // Ask the user to enter a word based on the placeholder
-                if (placeholder == "noun") {
+                if (placeholder == "noun")
+                {
                     cout << "Please enter a noun: ";
-                } else if (placeholder == "verb") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "verb")
+                {
                     cout << "Please enter a verb: ";
-                } else if (placeholder == "adjective") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "adjective")
+                {
                     cout << "Please enter an adjective: ";
-                } else if (placeholder == "proper noun") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "proper_noun")
+                {
                     cout << "Please enter a proper noun: ";
-                } else if (placeholder == "animal") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "animal")
+                {
                     cout << "Please enter an animal: ";
-                } else if (placeholder == "color") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "color")
+                {
                     cout << "Please enter a color: ";
-                } else if (placeholder == "adverb") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "adverb")
+                {
                     cout << "Please enter an adverb: ";
-                } else if (placeholder == "number") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "number")
+                {
                     cout << "Please enter a number: ";
-                } else if (placeholder == "measure of time") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "measure_of_time")
+                {
                     cout << "Please enter a measure of time: ";
-                } else if (placeholder == "silly word") {
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "silly_word")
+                {
                     cout << "Please enter a silly word: ";
-                } else if (placeholder == "noun - animal"){
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "noun_animal")
+                {
                     cout << "Please enter an animal: ";
-                } else if (placeholder == "verb, past tense"){
+                    cin >> replacement;
+                    cin.ignore();
+                }
+                else if (placeholder == "verb_past_tense")
+                {
                     cout << "Please enter a verb, past tense: ";
+                    cin >> replacement;
+                    cin.ignore();
                 }
 
-                cin.ignore();
-                getline(cin, replacement);
-
                 // Replace the placeholder with the user's input
-                line = regex_replace(line, regex(placeholder), replacement);
+                line = regex_replace(line, regex("\\(" + placeholder + "\\)"), replacement);
+                line = regex_replace(line, regex("\\(\\)"), ""); // Remove any leftover parentheses
             }
         }
 
